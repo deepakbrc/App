1643260729f9d63bc10c788c4029025560e8e0fd
deepakbrc <45401985+deepakbrc@users.noreply.github.com>
2019-07-06 18:56:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,117 +1,122 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import 'bulma/bulma';
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Container from 'react-bootstrap/Container'
 
-class App extends Component {
+
+
+class App extends React.Component {
     constructor(props) {
-        super(props) ;
-        this.state = {
-            list: [
-                "abc",
-                "def",
-                "ghi",
-                "jkl",
-                "ade",
-                "ace"
-            ]
-        }
+      super(props);
+      this.state = {
+        bar_checkin : "",
+        date_checkin : "",
+        card_checkout : "",
+        bar_checkout : "",
+        date_checkout: ""
+      };
+      this.checkin = this.checkin.bind(this);
+      this.checkout= this.checkout.bind(this);
+      this.handleBar_checkin= this.handleBar_checkin.bind(this);
+      this.handleDate_checkin= this.handleDate_checkin.bind(this);
+      this.handleCard_checkout= this.handleCard_checkout.bind(this);
+      this.handleBar_checkout= this.handleBar_checkout.bind(this);
+      this.handleDate_checkout= this.handleDate_checkout.bind(this);
     }
-    
-    render(){
-        return (
-          <div className="content">
-            <div className="container">
-              <section className="section">
-                            <List items={this.state.list} delete={this.removeItem} />
-              </section>
-              <hr />
-              <section className="section">
-                <form className="form" id="addItemForm">
-                  {/* <input
-                    type="text"
-                    className="input"
-                    id="addInput"
-                    placeholder="Something that needs ot be done..."
-                  />
-                  <button className="button is-info" onClick={this.addItem}>
-                    Add Item
-                  </button> */}
-                </form>
-              </section>
-            </div>
-          </div>
-        );
-      }
-  }
 
-  class List extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            filtered: []
-        };
-        this.handleChange = this.handleChange.bind(this);
+    handleBar_checkin(e) {
+        this.setState({bar_checkin: e.target.value});
     }
-    
-    componentDidMount() {
-    this.setState({
-      filtered: this.props.items
-    });
-  }
+    handleDate_checkin(e) {
+        this.setState({date_checkin: e.target.value});
+    }
+    handleCard_checkout(e) {
+        this.setState({card_checkout: e.target.value});
+    }
+    handleBar_checkout(e) {
+        this.setState({bar_checkout: e.target.value});
+    }
+    handleDate_checkout(e) {
+        this.setState({date_checkout: e.target.value});
+    }
 
-  componentWillReceiveProps(nextProps) {
-    this.setState({
-      filtered: nextProps.items
-    });
-  }
-    
-    handleChange(e) {
-        // Variable to hold the original version of the list
-    let currentList = [];
-        // Variable to hold the filtered list before putting into state
-    let newList = [];
-        
-        // If the search bar isn't empty
-    if (e.target.value !== "") {
-            // Assign the original list to currentList
-      currentList = this.props.items;
-            
-            // Use .filter() to determine which items should be displayed
-            // based on the search terms
-      newList = currentList.filter(item => {
-                // change current item to lowercase
-        const lc = item.toLowerCase();
-                // change search term to lowercase
-        const filter = e.target.value.toLowerCase();
-                // check to see if the current list item includes the search term
-                // If it does, it will be added to newList. Using lowercase eliminates
-                // issues with capitalization in search terms and search content
-        return lc.includes(filter);
-      });
-    } else {
-            // If the search bar is empty, set newList to original task list
-      newList = this.props.items;
-    }
-        // Set the filtered state based on what our rules added to newList
-    this.setState({
-      filtered: newList
-    });
-  }
-    
+
+
+
+  
+  
     render() {
-        return (
-            <div>
-                <input type="text" className="input" onChange={this.handleChange} placeholder="Search..." />
-                    <ul>
-                        {this.state.filtered.map(item => (
-                            <li key={item}>
-                                {item}
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-        )
-    }
-}
+      return (
+          
+        <div className="content">
+        <h1>Book Circulation</h1>
+        <div align='right'><button onclick="logout()">logout</button> </div>
+        <hr />
+        <Container>
+            <Row>
+                <Col>
+                <div align='center'>
+                    <h2 align='center'> CHECKIN</h2>
+                    <form name='check_in'>
+                        Book Code <input type='text' name='bar_checkin' placeholder="Please enter book code" value={this.state.bar_checkin} onChange={this.handleBar_checkin} /><br/> <br/>
+                        Date <input type='text' name='date_checkin' placeholder="dd/mm/yy" value={this.state.date_checkin} onChange={this.handleDate_checkin} /><br/> <br/>
+                        <button type="button" onClick={this.checkin}>CHECKIN</button>  
+                        <br/><br/> 
+                        <br/><br/><br/>
+                    </form>
 
-  ReactDOM.render(<App />, document.getElementById('app'));
+                     </div>           
+                </Col>
+                
+                <Col>
+                <div align='center'>
+                    <h2 align='center'> CHECKOUT</h2>
+                    <form name='checkout'>
+                        Card No. <input type="text" name="card_checkout" placeholder="Enter card no." value={this.state.card_checkout} onChange={this.handleCard_checkout} /> <br/><br/>
+                        Book Code <input type='text' name='bar_checkout' placeholder="Please enter book code" value={this.state.bar_checkout} onChange={this.handleBar_checkout} /><br/> <br/>
+                        Date <input type='text' name='date_checkout' placeholder="dd/mm/yy" value={this.state.date_checkout} onChange={this.handleDate_checkout} /><br/> <br/>
+                        <button type="button" onClick={this.checkout}>CHECKOUT</button>  
+                        <br/><br/> 
+                        <br/><br/><br/>
+                    </form>
+
+                     </div>        
+                
+                </Col>
+
+
+            </Row>
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            </Container>          
+          
+        </div>
+        
+      );
+    }
+    checkin(){
+        alert("BOOK: " + this.state.bar_checkin + "DATE: "+ this.state.date_checkin);
+    }
+
+    checkout(){
+        alert("CARD: " +this.state.card_checkout+ "BOOK: " + this.state.bar_checkout + "DATE: "+ this.state.date_checkout);
+
+    }
+
+
+
+  }
+  
+
+
+  
+  ReactDOM.render(<App />, document.getElementById("app"));
